329f4d081d27ae1bb20015eadc19500b0b50471b
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-04-09 23:54:15 +0700

    Refactor String module

--- src/Tuffenuff/String.fs
+++ src/Tuffenuff/String.fs
@@ -1,60 +1,55 @@
-module Tuffenuff.String
+namespace Tuffenuff
 
-open System
+module internal String =
+    open System
+    
+    [<Literal>]
+    let empty = ""
+    [<Literal>]
+    let ws = " "
+    [<Literal>]
+    let tab = "    "
+    [<Literal>]
+    let q = "\""
+    
+    let eol = Environment.NewLine
+    let eol_slash = $"%s{ws}\\%s{eol}%s{tab}"
 
+    let quote (value: 'a) : string = $"%s{q}%s{string value}%s{q}"
 
-let eol = Environment.NewLine
+    let print (name: string) (value: string) : string = $"%s{name}%s{ws}%s{value}%s{eol}"
 
+    let printKV (key: string) (value: string) : string = $"%s{key}=%s{value}"
 
-let ws = " "
+    let printKVQ (key: string) (value: 'a) : string =
+        value
+        |> quote
+        |> printKV key
 
+    // ReSharper disable once FSharpRedundantParens
+    let printList (values: #(string seq)) : string =
+        if (values :> obj) :? string[] then
+            values |> Seq.map quote |> String.concat ", " |> sprintf "[ %s ]"
+        else
+            values |> String.concat ws
 
-let empty = String.Empty
+    let printFlag (name: string) (value: bool) : string =
+        if value then $"%s{ws}--%s{name}" else empty
 
+    let printParameter<'T> (name: string) (value: 'T option) : string =
+        if value.IsSome then
+            $"%s{ws}--%s{name}=%s{string value.Value}"
+        else
+            empty
 
-let eol_slash = $" \\%s{eol}    "
+    let printParameterQ<'T> (name: string) (value : 'T option) : string =
+        match value with
+        | Some s -> s |> quote |> Some
+        | _ -> None
+        |> printParameter name
 
+    let trim (value: string) : string = value.TrimStart [| ' '; '\n'; '\r' |]
 
-let quote value = $"%c{'"'}%s{string value}%c{'"'}"
+    let variable (name: string) : string = $"${{%s{name}}}"
 
-
-let print name value = $"%s{name} %s{value}%s{eol}"
-
-
-let printKV key value = $"%s{key}=%s{value}"
-
-
-let printKVQ key value = printKV key (quote value)
-
-
-let printList list =
-    if (list :> obj) :? string[] then
-        list |> Seq.map quote |> String.concat ", " |> sprintf "[ %s ]"
-    else
-        list |> String.concat ws
-
-
-let printFlag name value = if value then $" --%s{name}" else empty
-
-
-let printParameter<'T> name (value : 'T option) =
-    if value.IsSome then
-        $" --%s{name}=%s{string value.Value}"
-    else
-        empty
-
-
-let printParameterQ<'T> name (value : 'T option) =
-    match value with
-    | Some s -> s |> quote |> Some
-    | _ -> None
-    |> printParameter name
-
-
-let trim (text : string) = text.TrimStart (eol.ToCharArray ())
-
-
-let variable = sprintf "${%s}"
-
-
-let (~%) = variable
+    let (~%) (name: string) : string = variable name
