1d8a429ebece0c02d9804773a5f0bdd3a8ad92dd
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-04-13 20:54:00 +0700

    Update tests

--- tests/Tuffenuff.UnitTests/From.fs
+++ tests/Tuffenuff.UnitTests/From.fs
@@ -1,61 +1,138 @@
 module Tests.From
 
+open System
 open Expecto
+open Tuffenuff.String
 open Tuffenuff.DSL
-open Tuffenuff.Domain.Types
-
 
 [<Tests>]
 let tests =
     testList "FROM instruction tests" [
-        testCase "from scratch test"
+        testCase "from 'scratch' test"
         <| fun _ ->
-            let expected = FromInstruction.Create ("scratch") |> From |> Instruction
+            Expect.equal
+                (fresh |> render)
+                $"FROM scratch%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. 'scratch' keyword\n\
+                  4. EOL\n\n"
 
-            let actual = fresh
-
-            Expect.equal actual expected "Image must be 'scratch'"
-
-        testCase "from alpine:latest test"
+        testCase "from 'alpine:latest' test"
         <| fun _ ->
-            let expected = FromInstruction.Create ("alpine:latest") |> From |> Instruction
+            Expect.equal
+                (from "alpine:latest" |> render)
+                $"FROM alpine:latest%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. provided image reference\n\
+                  4. EOL\n\n"
 
-            let actual = from "alpine:latest"
-
-            Expect.equal actual expected "Image must be 'alpine:latest'"
-
-        testCase "stage test"
+        testCase "from empty test"
         <| fun _ ->
-            let expected =
-                {
-                    Image = "alpine:latest"
-                    Name = Some "builder"
-                    Platform = None
-                }
-                |> From
-                |> Instruction
+            Expect.throwsT<ArgumentException>
+                (fun _ -> from "" |> ignore)
+                "Image reference can not be null or empty"
 
-            let actual = stage "alpine:latest" "builder"
-
-            Expect.equal actual expected
-            <| "Image must be 'alpine:latest' and named 'builder'"
-
-        testCase "from test"
+        testCase "stage 'alpine:latest' 'builder' test"
         <| fun _ ->
-            let expected =
-                {
-                    Image = "python:latest"
-                    Name = Some "test"
-                    Platform = Some "linux/amd64"
-                }
-                |> From
-                |> Instruction
+            Expect.equal
+                (stage "alpine:latest" "builder" |> render)
+                $"FROM alpine:latest AS builder%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. image reference from first argument\n\
+                  4. whitespace\n\
+                  5. 'AS' keyword\n\
+                  6. whitespace\n\
+                  7. stage name from second argument\n\
+                  8. EOL\n\n"
 
-            let actual =
-                based "python:latest" {
-                    as' "test"
+        testCase "stage empty 'builder' test"
+        <| fun _ ->
+            Expect.throwsT<ArgumentException>
+                (fun _ -> stage "" "builder" |> ignore)
+                "Image reference can not be null or empty"
+
+        testCase "stage 'alpine:latest' empty test"
+        <| fun _ ->
+            Expect.throwsT<ArgumentException>
+                (fun _ -> stage "alpine:latest" "" |> ignore)
+                "Stage name can not be null or empty"
+
+        testCase "based 'python:3.13.3' as 'foo' platform 'linux/amd64' test"
+        <| fun _ ->
+            Expect.equal
+                (based "python:3.13.3" {
+                    as' "foo"
                     platform "linux/amd64"
-                }
+                 }
+                 |> render)
+                $"FROM --platform=\"linux/amd64\" python:3.13.3 AS foo%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. two minus characters\n\
+                  4. key-value with\n\
+                    4.1. 'platform' keyword\n\
+                    4.2. quoted target platform from argument of 'platform' command\n\
+                  5. whitespace\n\
+                  6. image reference from first argument\n\
+                  7. whitespace\n\
+                  8. 'AS' keyword\n\
+                  9. whitespace\n\
+                  10. stage name from argument of 'as'' command\n\
+                  11. EOL\n\n"
 
-            Expect.equal actual expected "Instructions must be equal"
+        testCase "based 'python:3.13.3' as 'foo' test"
+        <| fun _ ->
+            Expect.equal
+                (based "python:3.13.3" { as' "foo" } |> render)
+                $"FROM python:3.13.3 AS foo%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. image reference from first argument\n\
+                  4. whitespace\n\
+                  5. 'AS' keyword\n\
+                  6. whitespace\n\
+                  7. stage name from argument of 'as'' command\n\
+                  8. EOL\n\n"
+
+        testCase "based 'python:3.13.3' platform 'linux/amd64' test"
+        <| fun _ ->
+            Expect.equal
+                (based "python:3.13.3" { platform "linux/amd64" } |> render)
+                $"FROM --platform=\"linux/amd64\" python:3.13.3%s{eol}"
+                "String must contain\n\
+                  1. 'FROM' keyword\n\
+                  2. whitespace\n\
+                  3. two minus characters\n\
+                  4. key-value with\n\
+                    4.1. 'platform' keyword\n\
+                    4.2. quoted target platform from argument of 'platform' command\n\
+                  5. whitespace\n\
+                  6. image reference from first argument\n\
+                  7. EOL\n\n"
+
+        testCase "base empty test"
+        <| fun _ ->
+            Expect.throwsT<ArgumentException>
+                (fun _ -> based "" { () } |> ignore)
+                "Stage name can not be null or empty"
+
+        testCase "base 'python:3.13.3' as empty test"
+        <| fun _ ->
+            Expect.throwsT<ArgumentException>
+                (fun _ -> based "python:3.13.3" { as' "" } |> ignore)
+                "Alias can not be null or empty"
+
+        testCase "base python:3.13.3' as 'foo' platform empty test"
+        <| fun _ ->
+            Expect.throwsT<ArgumentException>
+                (fun _ -> based "python:3.13.3" { platform "" } |> ignore)
+                "Platform can not be empty"
     ]
